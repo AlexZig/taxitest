176e1fd463eabd55f77c8001a678b948a245a81c
AlexZig <62720497+AlexZig@users.noreply.github.com>
2022-08-25 14:07:58 +0300

    Add files via upload

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,5 +1,24 @@
-const swiper = new Swiper('.ways__list', {
-    slidesPerView: 2.5,
-    spaceBetween: 20,
+let LinksAll = document.querySelectorAll('.ways__item');
+let modelsAll = document.querySelectorAll('.models__item');
+let models = document.querySelector('.models');
+for (let i = 0; i < LinksAll.length; i++) {
+  console.log(i)
+  LinksAll[i].onclick = function () {
+    console.log(models)
+    models.classList.add('_active');
+    modelsAll[i].classList.add('_active');
+    models.onclick = function (i) {
+      models.classList.remove('_active');
+      modelsAll[i].classList.remove('_active');
+    }
+  }
+}
 
-  });
+
+
+
+const swiper = new Swiper('.ways__list', {
+  slidesPerView: 2.5,
+  spaceBetween: 20,
+
+});
