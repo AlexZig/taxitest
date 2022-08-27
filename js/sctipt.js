510bc516bb0df3200387c21fd227dbe9a6d14296
AlexZig <62720497+AlexZig@users.noreply.github.com>
2022-08-27 06:59:44 +0300

    Add files via upload

--- js/sctipt.js
+++ js/sctipt.js
@@ -11,6 +11,26 @@ for (let i = 0; i < LinksAll.length; i++) {
     }
   }
 }
+var isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
+
+if (isIOS) {
+
+    var canvasVideo = new CanvasVideoPlayer({
+        videoSelector: '.video',
+        canvasSelector: '.canvas',
+        timelineSelector: false,
+        autoplay: true,
+        makeLoop: true,
+        pauseOnClick: false,
+        audio: false
+    });
+
+}else {
+
+    // Use HTML5 video
+    document.querySelectorAll('.canvas')[0].style.display = 'none';
+
+}   
 
 
 
